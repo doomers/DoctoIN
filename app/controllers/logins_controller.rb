1663c6385f51218f20a020c4cb94cff042203270
rahul357 <rahul40800@gmail.com>
2016-11-16 22:53:53 +0530

    improved session schema

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,29 @@
+class LoginsController < ApplicationController
+
+def new
+
+end
+
+def create
+
+  doctor =Doctor.find_by(email: params[:email])
+  if doctor && doctor.authenticate(params[:password])
+    session[:doctor_id] = doctor.id
+  	flash[:success] = 'You are Logged in'
+  	redirect_to doctor
+
+  else
+
+  	flash.now[:danger] = 'Wrong email password combination'
+  	render 'new'
+
+  end
+end
+
+def destroy
+	session[:doctor_id] = nil
+	flash[:success]='You have logged out'
+	redirect_to new_doctors_path
+
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   resources :doctors
 
+  get '/login', to: 'logins#new'
+   post '/login', to: 'logins#create'
+   get '/logout', to: 'logins#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
